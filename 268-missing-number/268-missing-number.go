6b3fb4166cf3dd31e6ca42dbdea8762f710bfde8
Alexander Konovalov <46221547+shuryak@users.noreply.github.com>
2022-08-19 03:05:30 +0300

    Time: 26 ms (55.18%), Space: 6.4 MB (60.71%) - LeetHub

--- 268-missing-number/268-missing-number.go
+++ 268-missing-number/268-missing-number.go
@@ -1,9 +1,11 @@
 func missingNumber(nums []int) int {
-    var sum int
-    for _, num := range nums {
-        sum += num
+    var result int
+    
+    for i, num := range nums {
+        result ^= i ^ num
     }
     
-    // Ожидаемая сумма (арифметическая прогрессия) с n+1 элементом минус фактическая сумма
-    return (len(nums) + 1) * len(nums) / 2 - sum
+    result ^= len(nums)
+    
+    return result
 }
