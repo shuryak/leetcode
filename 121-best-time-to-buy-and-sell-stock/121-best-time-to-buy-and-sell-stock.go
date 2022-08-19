4f81d6ad14e57a9cc44dbda3eb5106a84c47eb20
Alexander Konovalov <46221547+shuryak@users.noreply.github.com>
2022-08-19 16:09:53 +0300

    Time: 201 ms (47.18%), Space: 8.8 MB (70.15%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
@@ -1,35 +1,15 @@
+// Жадный алгоритм O(n)
+
 func maxProfit(prices []int) int {
-	var profit int
-    maxIdx := findIndexOfMax(prices)
-	for idx, val := range prices {
-		if idx == len(prices)-1 {
-			break
-		}
-        if prices[idx] == prices[idx+1] {
-            continue
+    buy := prices[0]
+    maxProfit := 0
+    for i := 1; i < len(prices); i++ {
+        // Закупить лучше тогда, когда цена ниже
+        if buy > prices[i] {
+            buy = prices[i]
+        } else if prices[i] - buy > maxProfit { // Если закупать не выгодно, значит нужно проверить, выгодно ли продавать
+            maxProfit = prices[i] - buy
         }
-        if idx >= maxIdx {
-            maxIdx = idx+1+findIndexOfMax(prices[idx+1:])
-        }
-		pre := prices[maxIdx] - val
-		if pre > profit {
-			profit = pre
-		}
-	}
-
-	return profit
-}
-
-func findIndexOfMax(nums []int) int {
-	resVal := nums[0]
-	resIdx := 0
-
-	for i := 1; i < len(nums); i++ {
-		if nums[i] > resVal {
-			resVal = nums[i]
-			resIdx = i
-		}
-	}
-
-	return resIdx
+    }
+    return maxProfit
 }
