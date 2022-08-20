d2742a37f763575e769cf8303f9b2baeda17786a
Alexander Konovalov <46221547+shuryak@users.noreply.github.com>
2022-08-20 18:27:06 +0300

    Time: 220 ms (66.16%), Space: 9.7 MB (87.19%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.go
+++ 234-palindrome-linked-list/234-palindrome-linked-list.go
@@ -7,34 +7,43 @@
  */
 func isPalindrome(head *ListNode) bool {
     fast, slow := head, head
-    left := []*ListNode{}
-    
-    if head.Next == nil {
-        return true
-    }
-    
-    var length int
-    for head != nil {
-        length++
-        head = head.Next
-    }
     
     for fast != nil && fast.Next != nil {
-        left = append(left, slow)
         fast = fast.Next.Next
         slow = slow.Next
     }
     
-    node := left[len(left)-1].Next
-    for i := 0; node != nil; i++ {
-        if length % 2 == 1 && i == 0 {
-            node = node.Next
-        }
-        fmt.Println(i)
-        if node.Val != left[len(left)-1-i].Val {
+    halfLength := getLength(slow)
+    prev := slow
+    cur := slow.Next
+    
+    for cur != nil {
+        tmp := cur.Next
+        cur.Next = prev
+        prev = cur
+        cur = tmp
+    }
+    
+    left := head
+    right := prev
+    
+    for i := 0; i < halfLength; i++ {
+        if left.Val != right.Val {
             return false
         }
-        node = node.Next
+        left = left.Next
+        right = right.Next
     }
+    
     return true
+}
+
+func getLength(head *ListNode) int {
+    var length int
+    cur := head
+    for cur != nil {
+        cur = cur.Next
+        length++
+    }
+    return length
 }
